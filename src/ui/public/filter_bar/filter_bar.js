e762eb82156b7ab1b9f932ff669bc36f0c4dc5b4
zhangchen <zhangchen2@teld.cn>
2017-03-22 17:24:18 +0800

    filterbar 别名转换

--- src/ui/public/filter_bar/filter_bar.js
+++ src/ui/public/filter_bar/filter_bar.js
@@ -24,7 +24,7 @@ module.directive('filterBar', function (Private, Promise, getAppState) {
   return {
     restrict: 'E',
     template: template,
-    scope: {},
+    scope: { indexPattern: '=' },
     link: function ($scope, $el, attrs) {
       // bind query filter actions to the scope
       [
