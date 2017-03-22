cbdd6e6fdab7b60c2f46f738afb5ff5a4f78cdfd
zhangchen <zhangchen2@teld.cn>
2017-03-22 17:19:57 +0800

    别名转换过滤器 参考：src\ui\public\doc_table\components\table_header_zh_CN.html fieldAlias:indexPattern.fields

---
+++ src/ui/public/filters/field_alias.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import uiModules from 'ui/modules';
+//字段别名映射
+uiModules
+  .get('kibana')
+  .filter('fieldAlias', function (Private) {
+    return Private(fieldAliasFilterProvider);
+  });
+
+function fieldAliasFilterProvider() {
+  return alias;
+
+  function alias(str, fields) {
+    let returnValue = str;
+    if (fields) {
+      let field = fields.find(field => field.name === str);
+
+      returnValue = field.alias || field.name;
+    }
+    return returnValue;
+  }
+}
+
+export default fieldAliasFilterProvider;
