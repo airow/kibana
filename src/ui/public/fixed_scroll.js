fad959613899a9004b9155c5417cb6f3c80d5ddb
zhangchen <zhangchen2@teld.cn>
2018-09-15 10:47:11 +0800

    固定水平滚动条

--- src/ui/public/fixed_scroll.js
+++ src/ui/public/fixed_scroll.js
@@ -6,6 +6,118 @@ const SCROLLER_HEIGHT = 20;
 
 uiModules
 .get('kibana')
+.directive('fixedScroll2', function ($timeout) {
+  return {
+    restrict: 'A',
+    link: function ($scope, $el) {
+      let $window = $(window);
+      let $scroller = $('<div class="fixed-scroll-scroller">').height(SCROLLER_HEIGHT);
+
+
+      /**
+       * Remove the listeners bound in listen()
+       * @type {function}
+       */
+      let unlisten = _.noop;
+
+      /**
+       * Listen for scroll events on the $scroller and the $el, sets unlisten()
+       *
+       * unlisten must be called before calling or listen() will throw an Error
+       *
+       * Since the browser emits "scroll" events after setting scrollLeft
+       * the listeners also prevent tug-of-war
+       *
+       * @throws {Error} If unlisten was not called first
+       * @return {undefined}
+       */
+      function listen() {
+        if (unlisten !== _.noop) {
+          throw new Error('fixedScroll listeners were not cleaned up properly before re-listening!');
+        }
+
+        let blockTo;
+        function bind($from, $to) {
+          function handler() {
+            if (blockTo === $to) return (blockTo = null);
+            $to.scrollLeft((blockTo = $from).scrollLeft());
+          }
+
+          $from.on('scroll', handler);
+          return function () {
+            $from.off('scroll', handler);
+          };
+        }
+
+        unlisten = _.flow(
+          bind($el, $scroller),
+          bind($scroller, $el),
+          function () { unlisten = _.noop; }
+        );
+      }
+
+      /**
+       * Revert DOM changes and event listeners
+       * @return {undefined}
+       */
+      function cleanUp() {
+        unlisten();
+        $scroller.detach();
+        $el.css('padding-bottom', 0);
+      }
+
+      /**
+       * Modify the DOM and attach event listeners based on need.
+       * Is called many times to re-setup, must be idempotent
+       * @return {undefined}
+       */
+      function setup() {
+        cleanUp();
+
+        const containerWidth = $el.width();
+        const contentWidth = $el.prop('scrollWidth');
+        const containerHorizOverflow = contentWidth - containerWidth;
+
+        const elTop = $el.offset().top - $window.scrollTop();
+        const elBottom = elTop + $el.height();
+        const windowVertOverflow = elBottom - $window.height();
+
+        const requireScroller = containerHorizOverflow > 0 && windowVertOverflow > 0;
+        if (!requireScroller) return;
+
+        // push the content away from the scroller
+        $el.css('padding-bottom', SCROLLER_HEIGHT);
+
+        // fill the scroller with a dummy element that mimics the content
+        $scroller
+        .width(containerWidth)
+        .html($('<div>').css({ width: contentWidth, height: SCROLLER_HEIGHT }))
+        .insertAfter($el);
+
+        // listen for scroll events
+        listen();
+
+        //2017-03-26 结果集少，横向双滚动条
+        //$el.css("overflow-x",'hidden');
+      }
+
+      // reset when the width or scrollWidth of the $el changes
+      $scope.$watchMulti([
+        function () { return $el.prop('scrollWidth'); },
+        function () { return $el.width(); }
+      ], setup);
+
+      // cleanup when the scope is destroyed
+      $scope.$on('$destroy', function () {
+        cleanUp();
+        $scroller = $window = null;
+      });
+    }
+  };
+});
+
+uiModules
+.get('kibana')
 .directive('fixedScroll', function ($timeout) {
   return {
     restrict: 'A',
@@ -98,7 +210,7 @@ uiModules
         listen();
 
         //2017-03-26 结果集少，横向双滚动条
-        $el.css("overflow-x",'hidden');
+        //$el.css("overflow-x",'hidden');
       }
 
       // reset when the width or scrollWidth of the $el changes
@@ -106,6 +218,20 @@ uiModules
         function () { return $el.prop('scrollWidth'); },
         function () { return $el.width(); }
       ], setup);
+
+      $scope.$watchMulti([
+        function () { return $el.offset().top; },
+        function () { return $(window).height(); },
+        function () { return $el.find('.kbn-table').height(); }
+      ], function (newValue, oldValue) {
+        var h = $(window).height() - newValue[0];
+        var th = $el.find('.kbn-table').height();
+        if (h > th) {
+          $el.height(h);
+        } else {
+          $el.height('auto');
+        }
+      });
 
       // cleanup when the scope is destroyed
       $scope.$on('$destroy', function () {
