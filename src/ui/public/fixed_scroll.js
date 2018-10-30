1613950c85c4380e8f91a38ef5d0fc85d6708053
zhangchen <zhangchen2@teld.cn>
2018-10-30 18:26:21 +0800

    修复IOS中嵌入TELD平台白屏的问题

--- src/ui/public/fixed_scroll.js
+++ src/ui/public/fixed_scroll.js
@@ -219,20 +219,42 @@ uiModules
         function () { return $el.width(); }
       ], setup);
 
-      $scope.$watchMulti([
-        //*该行会影响dashboard页面的显示*/function () { return $el.offset().top; },
-        function () { return _.startsWith(window.location.hash, '#/discover/') ? $el.offset().top : $(window).height(); },
-        function () { return $(window).height(); },
-        function () { return $el.find('.kbn-table').height(); }
-      ], function (newValue, oldValue) {
-        var h = $(window).height() - newValue[0];
-        var th = $el.find('.kbn-table').height();
-        if (h > th) {
-          $el.height(h);
-        } else {
-          $el.height('auto');
-        }
-      });
+
+      var browser = {
+        versions: function () {
+          var u = navigator.userAgent, app = navigator.appVersion;
+          return {         //移动终端浏览器版本信息
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+            iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+            iPad: u.indexOf('iPad') > -1, //是否iPad
+            webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+          };
+        }(),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+      };
+
+      if (false === browser.versions.iPhone && false === browser.versions.iPad) {
+        $scope.$watchMulti([
+          //*该行会影响dashboard页面的显示*/function () { return $el.offset().top; },
+          function () { return _.startsWith(window.location.hash, '#/discover/') ? $el.offset().top : $(window).height(); },
+          function () { return $(window).height(); },
+          function () { return $el.find('.kbn-table').height(); }
+        ], function (newValue, oldValue) {
+          var h = $(window).height() - newValue[0];
+          var th = $el.find('.kbn-table').height();
+          if (h > th) {
+            $el.height(h);
+          } else {
+            $el.height('auto');
+          }
+        });
+      }
 
       // cleanup when the scope is destroyed
       $scope.$on('$destroy', function () {
